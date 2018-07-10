277324650b37494c41e7fc9c61f9963fd5cf9767
Jamie Stevens <ste616@gmail.com>
2018-07-10 16:47:58 +1000

    Made a bunch of changes to the scheduler, and also to the scripts which can output all the flux densities or models.

--- database/atcacaldb_getallfluxdensities.pl
+++ database/atcacaldb_getallfluxdensities.pl
@@ -8,10 +8,11 @@ use strict; use warnings;
 
 my @cals = CalDB::Measurement->search_allfluxdensities();
 
-print "source_name  observation_mjd_start  observation_mjd_end  observation_mjd_integration  frequency_band  epoch_id  band_fluxdensity  band_fluxdensity_frequency\n";
+print "source_name  rightascens   declination  observation_mjd_start  observation_mjd_end  observation_mjd_integration  frequency_band  epoch_id  band_fluxdensity  band_fluxdensity_frequency   fluxdensity_scatter\n";
 for (my $i=0; $i <= $#cals; $i++) {
-    printf "%11s  %21s  %19s  %27s  %14s  %8s  %16s  %26s\n",$cals[$i]->source_name,
+    printf "%11s  %11s  %12s  %21s  %19s  %27s  %14s  %8s  %16s  %26s  %20s\n",$cals[$i]->source_name,
+    $cals[$i]->rightascension, $cals[$i]->declination,
     $cals[$i]->observation_mjd_start,$cals[$i]->observation_mjd_end,$cals[$i]->observation_mjd_integration,
     $cals[$i]->frequency_band,$cals[$i]->epoch_id,$cals[$i]->band_fluxdensity,
-    $cals[$i]->band_fluxdensity_frequency;
+    $cals[$i]->band_fluxdensity_frequency, $cals[$i]->fluxdensity_fit_scatter;
 }
